db04e391b46b16c97054b838455f0abd7001ee89
박우림 <woopark@gmarket.com>
2023-04-19 05:13:42 +0900

    주석 한글로 변경

--- js/start_onlyyou.js
+++ js/start_onlyyou.js
@@ -1,112 +1,145 @@
+/**
+ * 주석이 다소 어색할 수 있습니다. 중국어(간체)의 파파고 번역을 복붙했습니다.
+ * 해당 파일에서 특별히 수정이 필요한 건 없습니다.
+ */
 
+function init_onlyyou() {
+  // 두 div의 높이를 초기화
+  $("#div_onlyyou").css({ height: $(window).height() + 260 + "px" });
+  $("#div_oy_inner").css({ height: $(window).height() + 260 + "px" });
 
-        function init_onlyyou(){
-            // 初始化两个div的高度
-            $("#div_onlyyou").css({"height":$(window).height()+260+"px"});
-            $("#div_oy_inner").css({"height":$(window).height()+260+"px"});
+  // 사용자 정의 배경 설정
+  var start_bg_img = start_content["bg_img"];
+  if (
+    typeof start_content["bg_style"] != "undefined" &&
+    start_content["bg_style"] == "bg_custom"
+  ) {
+    if (typeof start_bg_img != "undefined" && start_bg_img != "") {
+      $("#div_onlyyou").css({
+        "background-image": "url(" + start_bg_img + ")",
+      });
+    }
+  }
 
-            // 设置自定义背景
-            var start_bg_img=start_content['bg_img'];
-            if(typeof(start_content['bg_style'])!='undefined' && start_content['bg_style']=='bg_custom'){
-                if(typeof(start_bg_img)!='undefined' && start_bg_img!=''){
-                    $("#div_onlyyou").css({"background-image": 'url('+start_bg_img+')'});
-                }
-            }
+  if (
+    typeof start_content["chase_title"] != "undefined" &&
+    start_content["chase_title"] != ""
+  ) {
+    $(".div_oy_text h1").html(start_content["chase_title"]); // 사용자 정의 내용 불러오기
+  } else {
+    $(".div_oy_text h1").html("做我女朋友好不好"); //기본값 설정
+  }
 
+  if (
+    typeof start_content["chase_text"] != "undefined" &&
+    start_content["chase_text"] != ""
+  ) {
+    $(".div_oy_text .p_oy_text").html(start_content["chase_text"]); // 사용자 정의 내용 불러오기
+  } else {
+    // 기본값 설정
+    $(".div_oy_text .p_oy_text").html(
+      "小可爱，我喜欢你好久了。不只是遇见你，即使只是想起你，都会让我小鹿突突地乱撞。我一定会好好地疼你，你做我女朋友好不好？"
+    );
+  }
 
-            if(typeof(start_content['chase_title'])!='undefined' && start_content['chase_title']!=''){
-                $('.div_oy_text h1').html(start_content['chase_title']); //加载自定义内容
-            }else{
-                $('.div_oy_text h1').html('做我女朋友好不好'); //设置默认值
-            }
+  // 본문 설정 전 사진
+  if (
+    typeof start_content["img_bool"] != "undefined" &&
+    start_content["img_bool"] == "img_true"
+  ) {
+    // 사진을 설정해 놓으면 뜬다
+    if (
+      typeof start_content["img_src"] != "undefined" &&
+      start_content["img_src"] != ""
+    ) {
+      $(".img_oy_text").attr("src", start_content["img_src"]);
+    }
+  }
+  if (
+    typeof start_content["img_bool"] == "undefined" ||
+    typeof start_content["chase_text"] == "undefined"
+  ) {
+    var random_img = random_img_as();
+    $(".img_oy_text").attr("src", random_img);
+  }
+}
 
-            if(typeof(start_content['chase_text'])!='undefined' && start_content['chase_text']!=''){ 
-                $('.div_oy_text .p_oy_text').html(start_content['chase_text']); //加载自定义内容
-            }else{ //设置默认值
-                $('.div_oy_text .p_oy_text').html('小可爱，我喜欢你好久了。不只是遇见你，即使只是想起你，都会让我小鹿突突地乱撞。我一定会好好地疼你，你做我女朋友好不好？');
-            }
-            
-            // 设置正文之前的照片
-            if(typeof(start_content['img_bool'])!='undefined' && start_content['img_bool']=='img_true'){ //如果设置了照片就显示
-                if(typeof(start_content['img_src'])!='undefined' && start_content['img_src']!=''){
-                    $(".img_oy_text").attr('src', start_content['img_src']);
-                }
-            } 
-            if(typeof(start_content['img_bool'])=='undefined' || typeof(start_content['chase_text'])=='undefined'){
-                var random_img=random_img_as();
-                $(".img_oy_text").attr('src', random_img);
-            }
-        } 
+var array_oy_benefit;
+// 내용 불러오거나 기본값 설정
+if (typeof start_content["chase_benefit"] != "undefined") {
+  array_oy_benefit = start_content["chase_benefit"];
+  // array_oy_benefit = array_oy_benefit.filter(function (s) {
+  //     return s && s.trim(); // 빈 값을 없애다
+  // }); // 빈 작품이지만 키의 경우를 정의했다.
+  if (array_oy_benefit[0] == "") {
+    array_oy_benefit[0] = "我会把全部工资都给你";
+  }
+  if (array_oy_benefit[1] == "") {
+    array_oy_benefit[1] = "每天做好吃的给你";
+  }
+  if (array_oy_benefit[2] == "") {
+    array_oy_benefit[2] = "你睡不着时给你讲故事";
+  }
+  if (array_oy_benefit[3] == "") {
+    array_oy_benefit[3] = "给你自由去做喜欢的事情";
+  }
+} else {
+  array_oy_benefit = [
+    "我会把全部工资都给你",
+    "每天做好吃的给你",
+    "你睡不着时给你讲故事",
+    "给你自由去做喜欢的事情",
+  ];
+}
+console.log(array_oy_benefit);
+var index_text_oy = 0;
 
+var count_text_oy = array_oy_benefit.length;
+console.log("一共有" + count_text_oy + "条件");
+function oy_show_benefit() {
+  var oy_text_height = $(".div_oy_text").height();
+  if (index_text_oy < count_text_oy) {
+    console.log("now the index_benefit_oy is->" + index_text_oy);
+    console.log("now the benefit_oy is->" + array_oy_benefit[index_text_oy]);
+    $(".li_oy_benefit")
+      .eq(index_text_oy)
+      .html(array_oy_benefit[index_text_oy])
+      .show();
+    if ($(document).height() - oy_text_height < 520) {
+      // 글자가 많아짐에 따라 실시간으로 높이 조절
+      $("#div_onlyyou").css({ height: $(document).height() + 160 + "px" });
+      $("#div_oy_inner").css({ height: $(document).height() + "px" });
+      console.log("update the document height +120");
+    }
+    index_text_oy++;
+  } else {
+    oy_show_note();
+  }
+}
 
-        var array_oy_benefit; 
-        //加载内容或设置默认值
-        if(typeof(start_content['chase_benefit'])!='undefined'){
-            array_oy_benefit=start_content['chase_benefit']; 
-            // array_oy_benefit = array_oy_benefit.filter(function (s) {
-            //     return s && s.trim(); // 去掉空值
-            // }); //全空作品，但是却定义了key的情况
-            if(array_oy_benefit[0]==''){
-                array_oy_benefit[0]='我会把全部工资都给你';
-            }
-            if(array_oy_benefit[1]==''){
-                array_oy_benefit[1]='每天做好吃的给你';
-            }
-            if(array_oy_benefit[2]==''){
-                array_oy_benefit[2]='你睡不着时给你讲故事';
-            }
-            if(array_oy_benefit[3]==''){
-                array_oy_benefit[3]='给你自由去做喜欢的事情';
-            }            
-        }else{
-            array_oy_benefit=['我会把全部工资都给你','每天做好吃的给你','你睡不着时给你讲故事','给你自由去做喜欢的事情'];
-        }
-        console.log(array_oy_benefit); 
-        var index_text_oy=0; 
+function oy_show_note() {
+  $("#div_oy_note").show();
+}
 
-        var count_text_oy=array_oy_benefit.length;
-        console.log('一共有'+count_text_oy+'条件');
-        function oy_show_benefit(){
-            var oy_text_height=$(".div_oy_text").height();
-            if(index_text_oy<count_text_oy){  
-                console.log('now the index_benefit_oy is->'+index_text_oy);                
-                console.log('now the benefit_oy is->'+array_oy_benefit[index_text_oy]);
-                $(".li_oy_benefit").eq(index_text_oy).html(array_oy_benefit[index_text_oy]).show();
-                if($(document).height()-oy_text_height<520){ //随着文字的增多，实时调整高度
-                    $("#div_onlyyou").css({"height":$(document).height()+160+"px"});
-                    $("#div_oy_inner").css({"height":$(document).height()+"px"});
-                    console.log('update the document height +120');
-                } 
-                index_text_oy++;
-            } else{
-                oy_show_note();
-            }
-        }
+function oy_hide_note() {
+  $("#div_oy_note").hide();
+}
 
-        function oy_show_note(){
-            $("#div_oy_note").show(); 
-        }
+function oy_go_next() {
+  $("#div_oy_yes").show();
+  setTimeout(function () {
+    $("#div_onlyyou").fadeOut();
+    init_theme();
+  }, 2000);
+  setTimeout(function () {
+    $("#div_onlyyou").remove();
+  }, 3000);
+}
 
-        function oy_hide_note(){
-            $("#div_oy_note").hide(); 
-        }
-
-        function oy_go_next(){  
-            $("#div_oy_yes").show();
-            setTimeout(function(){                
-                $('#div_onlyyou').fadeOut();
-                init_theme(); 
-            },2000);
-            setTimeout(function(){ 
-                $('#div_onlyyou').remove();
-            },3000);
-        }
-
-
-
-    function random_img_as(){  //获取随机的模板图片
-        // console.log('random_img_as'); 
-        var random_num=Math.floor(Math.random()*(array_as_pics_s.length)); //随机取值 
-        var random_img=array_as_pics_s[random_num];
-        return random_img;
-    } 
+function random_img_as() {
+  // 템플릿 그림 무작위로 가져오기
+  // console.log('random_img_as');
+  var random_num = Math.floor(Math.random() * array_as_pics_s.length); //  무작위로 값을 취하다.
+  var random_img = array_as_pics_s[random_num];
+  return random_img;
+}
--- js/theme_common.js
+++ js/theme_common.js
@@ -1,3 +1,11 @@
+/**
+ * 주석이 다소 어색할 수 있습니다. 중국어(간체)의 파파고 번역을 복붙했습니다.
+ * 해당 파일에서 특별히 수정이 필요한 건 없습니다.
+ * 굳이 수정 한다면 아래의 "재생중", "중지함" 메세지일 것입니다.
+ */
+const musicPlayMessage = "재생중";
+const musicPauseMessage = "중지함";
+
 var audio_music = document.getElementById("audio_music");
 var audio_record = document.getElementById("audio_record");
 
@@ -11,7 +19,7 @@ if (
     music_json["m_online_url"] != "null" &&
     music_json["m_online_url"] != ""
   ) {
-    //选择在线列表
+    // 온라인 목록 선택
     $("#audio_music").attr("src", music_json["m_online_url"]);
   }
   if (
@@ -19,14 +27,14 @@ if (
     music_json["m_upload_url"] != "null" &&
     music_json["m_upload_url"] != ""
   ) {
-    //选择在线列表并且上传了歌曲
+    // 온라인 리스트를 선택해서 노래를 올렸거든요
     $("#audio_music").attr("src", music_json["m_upload_url"]);
   }
   if (
     music_json["music_select"] == "m_upload" &&
     (music_json["m_upload_url"] == "null" || music_json["m_upload_url"] == "")
   ) {
-    //选择在线列表但是没有上传歌曲
+    // 온라인 목록을 선택했는데 노래가 안 올라왔어요
     console.log(
       "music_select m_upload but m_upload_url is null, set defaulted music"
     );
@@ -34,18 +42,18 @@ if (
     $("#audio_music").attr("src", random_music);
   }
   if (theme != "audio_list" || (theme == "audio_list" && start_id != "null")) {
-    audio_music.play(); //触发音乐自动播放
+    audio_music.play(); //자동으로 음악 재생하기
   } else {
     audio_music.pause();
     console.log("audio_list && no start");
   }
 } else {
-  //全新作品或空作品
+  // 새로운 작품 혹은 빈 작품
   console.log("set random music");
   var random_music = random_music_as();
   $("#audio_music").attr("src", random_music);
   if (theme != "audio_list" || (theme == "audio_list" && start_id != "null")) {
-    audio_music.play(); //触发音乐自动播放
+    audio_music.play(); //자동으로 음악 재생하기
   } else {
     audio_music.pause();
     console.log("audio_list && no start");
@@ -62,66 +70,66 @@ if (
     record_json["r_wechat_url"] != "null" &&
     record_json["r_wechat_url"] != ""
   ) {
-    //选择要语音
+    // 음성 선택
     $("#audio_record").attr("src", record_json["r_wechat_url"]);
   }
   if (
     record_json["record_bool"] == "r_true" &&
     (record_json["r_wechat_url"] == "null" || record_json["r_wechat_url"] == "")
   ) {
-    //选择要语音，但却没有录语音
-    $("#div_record").hide(); //不显示
+    //음성 선택, 음성을 녹음하지 않았어요
+    $("#div_record").hide(); // 보이지 않음
     $("#div_record_tips").hide();
   }
   if (record_json["record_bool"] == "r_false") {
-    //如果不要语音则不显示
+    // 음성을 사용하지 않으면 표시되지 않음
     $("#div_record").hide();
     $("#div_record_tips").hide();
   }
 } else {
   if (theme_content["bool_save"] == false) {
-    //全新作品或未保存内容，且未定义语音
+    // 새로운 작품 또는 저장되지 않은 내용, 음성이 정의되지 않음
     console.log("set random record");
     $("#audio_record").attr(
       "src",
       "http://cdn.aitetu520.com/chongqin_shenlin.mp3"
     );
   } else {
-    //非全新作品或已保存内容，但未定义语音，则不显示语音
+    // 새로운 작품이 아니거나 저장된 내용이지만 음성이 정의되지 않은 경우 음성이 표시되지 않습니다
     $("#div_record").hide();
     $("#div_record_tips").hide();
   }
 }
 
 function random_music_as() {
-  //获取随机的模板图片
+  // 템플릿 그림 무작위로 가져오기
   // console.log('random_words_as');
   var random_num = Math.floor(Math.random() * array_as_music.length); //随机取值
   var random_music = array_as_music[random_num];
   return random_music;
 }
 
-//控制音乐切换播放暂停
+// 음악 재생 일시 중지 제어
 var img_music = document.getElementById("img_music");
 var timeout_music;
 function music_switch() {
-  //切换
+  // 화면을 빠르게 바꾸다. 전환하다.
   clearTimeout(timeout_music);
   if (audio_music.paused) {
     console.log("switch music to play");
     audio_music.play();
-    audio_record.pause(); //播放音乐时录音一定暂停
+    audio_record.pause(); // 음악을 재생할 때 녹음은 반드시 일시 정지한다.
     img_music.style.webkitAnimation = "music_play_rotate 1s linear infinite";
-    $(".div_music_tips").html("재생중").show();
+    $(".div_music_tips").html(musicPlayMessage).show();
     timeout_music = setTimeout(function () {
       $(".div_music_tips").hide();
     }, 2500);
   } else {
     console.log("switch music to paused");
     audio_music.pause();
-    // audio_record.play(); //
+    // audio_record.play();
     img_music.style.webkitAnimation = "";
-    $(".div_music_tips").html("중지함").show();
+    $(".div_music_tips").html(musicPauseMessage).show();
     timeout_music = setTimeout(function () {
       $(".div_music_tips").hide();
     }, 2500);
@@ -131,7 +139,7 @@ function music_switch() {
 var timeout_record;
 var div_record = document.getElementById("div_record");
 function record_switch() {
-  //切换
+  // 화면을 빠르게 바꾸다. 전환하다.
   clearTimeout(timeout_record);
   if (audio_record.paused) {
     console.log("switch record to play");
@@ -139,17 +147,17 @@ function record_switch() {
     audio_music.pause(); //
     img_music.style.webkitAnimation = "";
     div_record.style.webkitAnimation = "btn_rotate 1s linear infinite";
-    $(".div_record_tips").html("재생중").show();
+    $(".div_record_tips").html(musicPlayMessage).show();
     timeout_record = setTimeout(function () {
       $(".div_record_tips").hide();
     }, 2500);
   } else {
     console.log("switch record to pause");
-    audio_record.pause(); //播放音乐时录音一定暂停
+    audio_record.pause(); // 음악을 재생할 때 녹음은 반드시 일시 정지한다.
     audio_music.play();
     img_music.style.webkitAnimation = "music_play_rotate 1s linear infinite";
     div_record.style.webkitAnimation = "";
-    $(".div_record_tips").html("중지함").show();
+    $(".div_record_tips").html(musicPauseMessage).show();
     timeout_record = setTimeout(function () {
       $(".div_record_tips").hide();
     }, 2500);
@@ -158,7 +166,7 @@ function record_switch() {
 
 wx.ready(function () {
   console.log("wx.ready success to start");
-  audio_music.play(); //触发音乐自动播放
+  audio_music.play(); // 자동으로 음악 재생하기
   wx.checkJsApi({
     jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"], // 需要检测的JS接口列表，所有JS接口列表见附录2,
     success: function (res) {
@@ -167,7 +175,7 @@ wx.ready(function () {
         theme != "audio_list" ||
         (theme == "audio_list" && start_id != "null")
       ) {
-        audio_music.play(); //触发音乐自动播放
+        audio_music.play(); // 자동으로 음악 재생하기
       } else {
         audio_music.pause();
         console.log("audio_list && no start");
@@ -179,7 +187,7 @@ wx.ready(function () {
         theme != "audio_list" ||
         (theme == "audio_list" && start_id != "null")
       ) {
-        audio_music.play(); //触发音乐自动播放
+        audio_music.play(); // 자동으로 음악 재생하기
       } else {
         audio_music.pause();
         console.log("audio_list && no start");
@@ -192,14 +200,14 @@ wx.error(function (res) {
   console.log("wx.error -> " + res);
   audio_music.play();
   wx.checkJsApi({
-    jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+    jsApiList: ["updateAppMessageShareData", "updateTimelineShareData"], // 검사가 필요한 JS 인터페이스 목록, 모든 JS 인터페이스 목록은 부록 2와 같다.
     success: function (res) {
       console.log("wx.checkJsApi success");
       if (
         theme != "audio_list" ||
         (theme == "audio_list" && start_id != "null")
       ) {
-        audio_music.play(); //触发音乐自动播放
+        audio_music.play(); // 자동으로 음악 재생하기
       } else {
         audio_music.pause();
         console.log("audio_list && no start");
@@ -211,7 +219,7 @@ wx.error(function (res) {
         theme != "audio_list" ||
         (theme == "audio_list" && start_id != "null")
       ) {
-        audio_music.play(); //触发音乐自动播放
+        audio_music.play(); // 자동으로 음악 재생하기
       } else {
         audio_music.pause();
         console.log("audio_list && no start");
